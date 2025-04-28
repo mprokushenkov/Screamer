c075be49047d2edf9d4270313070d049f2adccf0
Mikhail Prokushenkov <m.prokushenkov@gmail.com>
2025-04-28 14:58:27 +0300

    Add idiomatic function 'Id'

---
+++ tests/Functional.Tests/IdiomsTests.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using static Bstm.Functional.Idioms;
+
+namespace Bstm.Functional.Tests
+{
+    public class IdiomsTests
+    {
+        [Theory]
+        [AutoData]
+        public void IdShouldReturnSubject(string[] ar) =>
+            // Fixture setup
+            // Exercise system
+            // Verity outcome
+            ar.OrderBy(Id).Should().BeEquivalentTo(ar);
+    }
+}
